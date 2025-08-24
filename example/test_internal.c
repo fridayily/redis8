b0ba34127c147e351c393142adad93f95934cb6f
fridayily <fridayily@163.com>
2025-08-24 23:57:38 +0800

    add internal test of dict

---
+++ example/test_internal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define REDIS_TEST 1
+#include "../src/listpack.c"
+#include "../src/dict.c"
+
+int main(int argc, char *argv[]) {
+    UNUSED(argc);
+    UNUSED(argv);
+    // printf("Starting listpack internal tests...\n");
+    // if (listpackTest(argc, argv, 0) == 0) {
+    //     printf("All listpack internal tests passed!\n");
+    //     return 0;
+    // } else  {
+    //     printf("Some listpack internal tests failed!\n");
+    //     return 1;
+    // }
+
+    if (dictTest(argc, argv, 0)==0) {
+        printf("All dict internal tests passed!\n");
+        return 1;
+    }else  {
+        printf("Some dict internal tests failed!\n");
+        return 1;
+    }
+}
