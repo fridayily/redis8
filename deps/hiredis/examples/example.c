394234e7c23d359225f79aebb614123a6d073706
fridayily <fridayily@163.com>
2025-11-10 00:23:15 +0800

    add comment of steam

--- deps/hiredis/examples/example.c
+++ deps/hiredis/examples/example.c
@@ -56,113 +56,6 @@ static void example_argv_command(redisContext *c, size_t n) {
 }
 
 
-static void example_stream(redisContext *c) {
-    redisReply *reply;
-    reply = redisCommand(c, "XADD mystream * sensor-id 1234 temperature 19.8");
-    if (reply->type == REDIS_REPLY_ERROR) {
-        printf("XADD 错误: %s\n", reply->str);
-    } else {
-        printf("添加消息 ID: %s\n", reply->str);
-    }
-    freeReplyObject(reply);
-    // 创建消费者组
-    reply = redisCommand(c, "XGROUP CREATE mystream mygroup $ MKSTREAM");
-    if (reply->type == REDIS_REPLY_ERROR) {
-        printf("XGROUP CREATE 错误: %s\n", reply->str);
-    } else {
-        printf("消费者组创建成功\n");
-    }
-    freeReplyObject(reply);
-
-    // 添加更多测试数据
-    reply = redisCommand(c, "XADD mystream * sensor-id 1235 temperature 20.1");
-    freeReplyObject(reply);
-
-    reply = redisCommand(c, "XADD mystream * sensor-id 1236 temperature 21.5");
-    freeReplyObject(reply);
-
-    // 从消费者组读取消息
-    reply = redisCommand(c, "XREADGROUP GROUP mygroup consumer1 COUNT 2 STREAMS mystream >");
-    if (reply->type == REDIS_REPLY_ARRAY) {
-        char **message_ids = malloc(sizeof(char*) * 10); // 假设最多10条消息
-        int msg_count = 0;
-        for (size_t i = 0; i < reply->elements; i++) {
-            redisReply *stream = reply->element[i];
-            printf("Stream: %s\n", stream->element[0]->str);
-
-            redisReply *messages = stream->element[1];
-
-            for (size_t j = 0; j < messages->elements; j++) {
-                redisReply *message = messages->element[j];
-                printf("  消息 ID: %s\n", message->element[0]->str);
-                // 保存消息ID用于后续确认
-                if (msg_count < 10) {
-                    message_ids[msg_count] = strdup(message->element[0]->str);
-                    msg_count++;
-                }
-                redisReply *fields = message->element[1];
-                for (size_t k = 0; k < fields->elements; k += 2) {
-                    printf("    %s: %s\n",
-                           fields->element[k]->str,
-                           fields->element[k+1]->str);
-                }
-            }
-        }
-        // 确认消息处理完成
-        if (msg_count > 0) {
-            // 构建 XACK 命令
-            char *cmd = malloc(256);
-            int offset = sprintf(cmd, "XACK mystream mygroup");
-            for (int i = 0; i < msg_count; i++) {
-                offset += sprintf(cmd + offset, " %s", message_ids[i]);
-            }
-
-            redisReply *ack_reply = redisCommand(c, cmd);
-            if (ack_reply->type != REDIS_REPLY_ERROR) {
-                printf("确认了 %lld 条消息\n", ack_reply->integer);
-            }
-            freeReplyObject(ack_reply);
-            free(cmd);
-
-            // 释放消息ID内存
-            for (int i = 0; i < msg_count; i++) {
-                free(message_ids[i]);
-            }
-        }
-        free(message_ids);
-    }
-    freeReplyObject(reply);
-
-    // 确认消息处理完成
-    // reply = redisCommand(c, "XACK mystream mygroup 1640995200000-0 1640995200001-0");
-    // if (reply->type != REDIS_REPLY_ERROR) {
-    //     printf("确认了 %lld 条消息\n", reply->integer);
-    // }
-    // freeReplyObject(reply);
-
-    reply = redisCommand(c, "XINFO STREAM mystream");
-    if (reply->type == REDIS_REPLY_ARRAY) {
-        for (size_t i = 0; i < reply->elements; i += 2) {
-            printf("%s: ", reply->element[i]->str);
-            redisReply *value = reply->element[i+1];
-            if (value->type == REDIS_REPLY_INTEGER) {
-                printf("%lld\n", value->integer);
-            } else if (value->type == REDIS_REPLY_STRING) {
-                printf("%s\n", value->str);
-            }
-        }
-    }
-    freeReplyObject(reply);
-
-    // 完全删除整个 stream
-    reply = redisCommand(c, "DEL mystream");
-    if (reply->type != REDIS_REPLY_ERROR) {
-        printf("已删除整个 stream\n");
-    }
-    freeReplyObject(reply);
-
-}
-
 int main(int argc, char **argv) {
     unsigned int j, isunix = 0;
     redisContext *c;
@@ -199,6 +92,7 @@ int main(int argc, char **argv) {
     reply = redisCommand(c,"PING");
     printf("PING: %s\n", reply->str);
     freeReplyObject(reply);
+    exit(0);
 
     /* Set a key */
     reply = redisCommand(c,"SET %s %s", "foo", "hello world");
@@ -245,7 +139,6 @@ int main(int argc, char **argv) {
 
     /* See function for an example of redisCommandArgv */
     example_argv_command(c, 10);
-    example_stream(c);
     /* Disconnects and frees the context */
     redisFree(c);
 
--- example/test_base.c
+++ example/test_base.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 void foo(int x,int y,int z)
 {
@@ -30,8 +32,36 @@ void endian_test()
     printf("i=%d",i);
 }
 
+void daemonize(void) {
+    // 返回进程id
+    pid_t pid = getpid();
+    // 返回 session_id
+    pid_t sid = getsid(0);
+    // 返回进程组ID
+    pid_t pgid = getpgid(0);
+    printf("pid %d\n",pid); // 997
+    printf("sid %d\n",sid); // 1
+    printf("pgid %d\n",pgid);  // 997
+    printf("------------------\n");
+    if (fork() != 0) exit(0); // 父进程退出
+    // 子进程继续执行
+    pid_t child_pid = getpid();
+    pid_t child_sid = getsid(0);
+    pid_t child_pgid = getpgid(0);
+    printf("child_pid %d\n",child_pid); // 1000
+    printf("child_sid %d\n",child_sid); // 1
+    printf("child_pgid %d\n",child_pgid); // 997
+    printf("------------------\n");
+    setsid();
+    pid_t new_pid = getpid();
+    pid_t new_sid = getsid(0);
+    pid_t new_pgid = getpgid(0);
+    printf("new_pid %d\n",new_pid); // 1000
+    printf("new_sid %d\n",new_sid); // 1000
+    printf("new_pgid %d\n",new_pgid); // 1000
+}
 
 int main() {
-    endian_test();
+    daemonize();
     return 0;
 }
